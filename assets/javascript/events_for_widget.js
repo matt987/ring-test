1f69d9b0b0a9afc349f26932edef0aab3b1924ec
Matt's <matt987@mathiasen.com>
2013-05-13 22:09:45 -0300

    Dispatched retry event

--- assets/javascript/events_for_widget.js
+++ assets/javascript/events_for_widget.js
@@ -1,42 +1,32 @@
+RingcaptchaEvents = {
+  widgedDisplayedEvent: new CustomEvent("onWidgetDisplayed", {
+    detail: {
+      name: "Widget displayed"
+    }
+  }),
 
-// Definimos el nuevo Evento
-document.body.addEventListener("onWidgetDisplayed", function(e) {
-	// Obtenemos como parámetro el Evento (e)
-	console.info("Event is: ", e);
-	
-	// Además, disponemos de información extra.
-	console.info("Custom data is: ", e.detail);
-});
+  //onSuccessfulPinRequestSent: inmediatamente luego de enviar el PIN exitosamente
+  successfulPinRequestSentEvent: new CustomEvent("onSuccessfulPinRequestSent", {
+    detail: {
+      name: "Successful pin request sent"
+    }
+  }),
 
+  //onRetryRequested: inmediatamente luego de que se clickee el boton de retry
+  retryRequestedEvent: new CustomEvent("onRetryRequested", {
+    detail: {
+      name: "Retry request event"
+    }
+  }),
 
-//onWidgetDisplayed: inmediatamente luego de mostrar el widget
-var widgedDisplayedEvent = new CustomEvent("onWidgetDisplayed", {
-  detail: {
-    name: "Widget displayed"
-  }
-});
+  //onInvalidNumberEntered: cuando un numero invalido fue ingresado
+  invalidNumberEntered: new CustomEvent("onInvalidNumberEntered", {
+    detail: {
+      name: "Invalid number entered"
+    }
+  }),
+};
 
-//onSuccessfulPinRequestSent: inmediatamente luego de enviar el PIN exitosamente
-var successfulPinRequestSentEvent = new CustomEvent("onSuccessfulPinRequestSent", {
-  detail: {
-    name: "Successful pin request sent"
-  }
-});
-
-//onRetryRequested: inmediatamente luego de que se clickee el boton de retry
-//Ringcaptcha.reload()
-var retryRequestedEvent = new CustomEvent("onRetryRequested", {
-  detail: {
-    name: "Retry request event"
-  }
-});
-
-//onInvalidNumberEntered: cuando un numero invalido fue ingresado
-var invalidNumberEntered = new CustomEvent("onInvalidNumberEntered", {
-  detail: {
-    name: "Invalid number entered"
-  }
-});
 
 // Ejecutamos el evento
 //document.body.dispatchEvent(myEvent);
